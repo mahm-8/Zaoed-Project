779806b5f11b95141a7c35282ce51cb76fea3bca
mahm <am18_8@outlook.com>
2023-12-20 20:27:53 +0300

    add card,information profile , cars

--- zaoed/lib/main.dart
+++ zaoed/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:zaoed/bookmark_screens/saved_bookmarks_screen.dart';
+import 'package:zaoed/onboarding/onboarding_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,8 +12,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
       supportedLocales: const [
         Locale("ar"),
@@ -22,7 +21,7 @@ class MainApp extends StatelessWidget {
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
-      home: SavedBookmarksScreen(),
+      home: OnboradingScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
