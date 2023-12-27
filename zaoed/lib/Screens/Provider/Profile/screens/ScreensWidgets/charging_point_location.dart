536b9810f290baacfcb43ebfdd054183e7c54c6e
wadha <143879896+Wadha21@users.noreply.github.com>
2023-12-27 11:52:13 +0300

    google map

--- zaoed/lib/Screens/Provider/Profile/screens/ScreensWidgets/charging_point_location.dart
+++ zaoed/lib/Screens/Provider/Profile/screens/ScreensWidgets/charging_point_location.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:zaoed/constants/imports.dart';
-
+import 'package:zaoed/constants/colors.dart';
 class ChargingPointLocation extends StatefulWidget {
   ChargingPointLocation({
     super.key,
